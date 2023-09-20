cc9f429f78bb3d0820fa1b8dd93e7f23eec62880
shruti166 <shruti.chemistry.lm@gmail.com>
2023-09-20 09:54:24 +0530

    added

--- Chapter-4/index.ts
+++
@@ -1,6 +0,0 @@
-//What are Higher Order Components ?
-
-
-//Using Children prop 
-
-// const Button = ({text}) => <button className="btn">{text}</button>
---
+++ Chapter-4/src/components/Geolocation.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+
+const GeoLocation = () => {
+    const [latitude, setLatitude] = useState<number | null>(null);
+    const [longitude, setLongitude] = useState<number | null>(null);
+}
+
+export default GeoLocation;
